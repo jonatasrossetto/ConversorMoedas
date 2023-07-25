4f3e0c8852c5ad8d7531cb12ff14da16147d46df
Jonatas Rossetto <jonatas.rossetto@gmail.com>
2023-07-25 16:46:20 -0300

    painel simples implementado

--- ConversorMoedas/src/conversor/gui/ConversorPainel.java
+++ ConversorMoedas/src/conversor/gui/ConversorPainel.java
@@ -3,14 +3,19 @@ package conversor.gui;
 import javax.swing.JPanel;
 import javax.swing.JLabel;
 import java.awt.Font;
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.swing.SwingConstants;
 import javax.swing.JList;
 import javax.swing.JComboBox;
 import javax.swing.JTextField;
+import javax.swing.JButton;
 
 public class ConversorPainel extends JPanel {
-	private JTextField textField;
-	private JTextField textField_1;
+	private JTextField textFieldValorMoedaOrigem;
+	private JTextField textFieldValorMoedaDestino;
+	private JTextField textFieldTaxaConversaoOrigemDestino;
 
 	/**
 	 * Create the panel.
@@ -31,14 +36,15 @@ public class ConversorPainel extends JPanel {
 		lblNewLabel_1.setBounds(10, 101, 45, 13);
 		add(lblNewLabel_1);
 		
-		String moedas[]= { "BRL","USD","EUR","GBP","ARS"};
-		JComboBox comboBox = new JComboBox(moedas);
-		comboBox.setBounds(10, 70, 86, 21);
-		add(comboBox);
+		String[] moedas = {"BRL","USD","EUR","GBP","ARS"};
 		
-		JComboBox comboBox_1 = new JComboBox(new Object[]{});
-		comboBox_1.setBounds(10, 124, 86, 21);
-		add(comboBox_1);
+		JComboBox comboBoxMoedaOrigem = new JComboBox(moedas);
+		comboBoxMoedaOrigem.setBounds(10, 70, 86, 21);
+		add(comboBoxMoedaOrigem);
+		
+		JComboBox comboBoxMoedaDestino = new JComboBox(moedas);
+		comboBoxMoedaDestino.setBounds(10, 124, 86, 21);
+		add(comboBoxMoedaDestino);
 		
 		JLabel lblNewLabel_1_1 = new JLabel("valor:");
 		lblNewLabel_1_1.setBounds(106, 74, 45, 13);
@@ -48,17 +54,41 @@ public class ConversorPainel extends JPanel {
 		lblNewLabel_1_1_1.setBounds(106, 128, 45, 13);
 		add(lblNewLabel_1_1_1);
 		
-		textField = new JTextField();
-		textField.setBounds(142, 71, 96, 19);
-		add(textField);
-		textField.setColumns(10);
+		textFieldValorMoedaOrigem = new JTextField();
+		textFieldValorMoedaOrigem.setBounds(142, 71, 96, 19);
+		add(textFieldValorMoedaOrigem);
+		textFieldValorMoedaOrigem.setColumns(10);
 		
-		textField_1 = new JTextField();
-		textField_1.setBounds(142, 125, 96, 19);
-		add(textField_1);
-		textField_1.setColumns(10);
+		textFieldValorMoedaDestino = new JTextField();
+		textFieldValorMoedaDestino.setBounds(142, 125, 96, 19);
+		add(textFieldValorMoedaDestino);
+		textFieldValorMoedaDestino.setColumns(10);
 		
+		JLabel lblNewLabel_2 = new JLabel("Resultado");
+		lblNewLabel_2.setBounds(10, 155, 86, 13);
+		add(lblNewLabel_2);
 		
+		JLabel labelDisplayResultado = new JLabel("texto com o resultado da conversão");
+		labelDisplayResultado.setBounds(10, 178, 430, 43);
+		add(labelDisplayResultado);
+		
+		textFieldTaxaConversaoOrigemDestino = new JTextField();
+		textFieldTaxaConversaoOrigemDestino.setBounds(331, 98, 96, 19);
+		add(textFieldTaxaConversaoOrigemDestino);
+		textFieldTaxaConversaoOrigemDestino.setColumns(10);
+		
+		JLabel lblNewLabel_3 = new JLabel("Taxa de Conversão");
+		lblNewLabel_3.setBounds(189, 101, 132, 13);
+		add(lblNewLabel_3);
+		
+		JButton btnConverter = new JButton("Converter");
+		btnConverter.setBounds(222, 151, 111, 21);
+		add(btnConverter);
+		
+		JButton btnLimpar = new JButton("Limpar");
+		btnLimpar.setBounds(342, 151, 85, 21);
+		add(btnLimpar);
 
+		
 	}
 }
--- ConversorMoedas/src/conversor/gui/Teste.java
+++ ConversorMoedas/src/conversor/gui/Teste.java
@@ -10,42 +10,13 @@ public class Teste {
 
 	public static void main(String[] args) {
 		
-//		JFrame f = new JFrame("frame");
-//		
-//		ConversorPainel c = new ConversorPainel();
-//		f.setSize(400,400);
-//		f.add(c);
-//		f.show();
+		JFrame f = new JFrame("frame");
 		
-		
-		 JFrame f = new JFrame("frame");
-         
-	   
-	        //create a panel
-	        JPanel p =new JPanel();
-	         
-	        //create a new label
-	        JLabel l= new JLabel("select the day of the week");
-	 
-	        //String array to store weekdays
-	        String week[]= { "Monday","Tuesday","Wednesday",
-	                         "Thursday","Friday","Saturday","Sunday"};
-	         
-	        //create list
-	        JComboBox b= new JComboBox(week);
-	         
-	        //set a selected index
-	        b.setSelectedIndex(2);
-	         
-	        //add list to panel
-	        p.add(b);
-	  
-	        f.add(p);
-	         
-	        //set the size of frame
-	        f.setSize(400,400);
-	          
-	        f.show();
+		ConversorPainel c = new ConversorPainel();
+		f.setSize(500,400);
+		f.add(c);
+		f.setVisible(true);
+
 		
 	}
 
